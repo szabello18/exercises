f3a8f3ca02b80d846fb264210f86d33bf20e6deb
Szablewski <szabello18@gmail.com>
2017-09-12 15:22:07 +0200

    second task completed

--- src/ex2.java
+++ src/ex2.java
@@ -1,7 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 /**
- * Created by Szablewksi on 9/12/2017.
+ * Created by Szablewksi on 9/11/2017.
  */
 public class ex2 {
+
+    public static void main(String[] args){
+
+        double employeePayment;
+
+        System.out.println("Specify how much you earn per month? ");
+        Scanner scanner = new Scanner(System.in);
+        employeePayment = scanner.nextDouble();
+
+        if (employeePayment <= 85528) {
+            double taxExpense = (employeePayment * 0.18) - 556.02;
+            System.out.println("Your tax is equal " + taxExpense);
+        }
+
+        if (employeePayment > 85528){
+            double bigTaxExpense = (employeePayment *0.32) - 14839.02;
+            System.out.println("Your tax is equal " + bigTaxExpense);
+        }
+    }
 }
