609f977dae2552b0f32b164ed701d3861272160a
Szablewski <szabello18@gmail.com>
2017-09-13 10:52:03 +0200

    new methods

--- src/Ex2.java
+++ src/Ex2.java
@@ -7,11 +7,13 @@ public class Ex2 {
 
     public static double employeePayment;
     public static final int taxLevel = 85528;
-    public static final double taxBase = 556.02;
-    public static final double taxFactor = 0.32;
+    public static final double normalTaxBase = 556.02;
+    public static final double bigTaxBase = 14839.02;
+    public static final double normalTaxFactor = 0.16;
+    public static final double bigTaxFactor = 0.32;
+
 
     public void getEmployeePayment(double employeePayment, int taxLevel){
-
     }
 
     public void calculateTaxExpense(double employeePayment, double taxBase, double taxFactor){
@@ -19,20 +21,17 @@ public class Ex2 {
 
     public static void main(String[] args){
 
-
-
-
         System.out.println("Specify how much you earn per month? ");
         Scanner scanner = new Scanner(System.in);
         employeePayment = scanner.nextDouble();
 
         if (employeePayment <= taxLevel) {
-            double taxExpense = (employeePayment * taxFactor) - taxBase;
+            double taxExpense = (employeePayment * normalTaxFactor) - normalTaxBase;
             System.out.println("Your tax is equal " + taxExpense);
         }
 
         if (employeePayment > taxLevel){
-            double bigTaxExpense = (employeePayment * taxFactor) - taxBase;
+            double bigTaxExpense = (employeePayment * bigTaxFactor) - bigTaxBase;
             System.out.println("Your tax is equal " + bigTaxExpense);
         }
     }
